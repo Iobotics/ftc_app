064288e4f3bcdd36ea3f5e0b61cbaaa67ef6ab61
Jeff Malins <jmalins@iolani.org>
2015-10-26 21:37:33 -1000

    added tankbot teleop, tester for K9

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -56,19 +56,21 @@ public class FtcOpModeRegister implements OpModeRegister {
      */
     manager.register("SuperK9Auto", SuperK9Auto.class);
     manager.register("SuperK9TeleOp", SuperK9TeleOp.class);
+    manager.register("SuperK9Test", SuperK9Test.class);
+    manager.register("TankbotTeleOp", TankbotTeleOp.class);
 
     manager.register("NullOp", NullOp.class);
 
-    manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-    manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
+    //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
+    //manager.register("K9TeleOp", K9TeleOp.class);
+    //manager.register ("PushBotAuto", PushBotAuto.class);
+    //manager.register ("PushBotManual", PushBotManual.class);
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
      */
 
-    manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
+    //manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
     //manager.register("ColorSensorDriver", ColorSensorDriver.class);
 
     //manager.register("IrSeekerOp", IrSeekerOp.class);
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SuperK9Test.java
@@ -0,0 +1,115 @@
+/* Copyright (c) 2014 Qualcomm Technologies Inc
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Qualcomm Technologies Inc nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+//import com.qualcomm.robotcore.hardware.Servo;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.DigitalChannelController;
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * TeleOp Mode
+ * <p>
+ * Enables control of the robot via the gamepad
+ */
+public class SuperK9Test extends OpMode {
+
+    // dead reckonining information //
+    final static int    ENCODER_TICKS_PER_REV = 1120; // Neverest 40
+    final static int    WHEEL_DIAMETER        = 6;    // inches / REV
+    final static double INCHES_PER_TICK       = (WHEEL_DIAMETER * Math.PI) / ENCODER_TICKS_PER_REV;
+
+    // hardware instances //
+    DcMotor _motorRightFront;
+    DcMotor _motorRightRear;
+    DcMotor _motorLeftFront;
+    DcMotor _motorLeftRear;
+
+    Servo _manServo;
+
+    OpticalDistanceSensor _sensorODS;
+    LightSensor _sensorLego;
+
+
+    @Override
+    public void init() {
+        _motorRightFront = hardwareMap.dcMotor.get("rightFront");
+        _motorRightRear  = hardwareMap.dcMotor.get("rightRear");
+        _motorLeftFront = hardwareMap.dcMotor.get("leftFront");
+        _motorLeftRear = hardwareMap.dcMotor.get("leftRear");
+
+        _motorRightFront.setDirection(DcMotor.Direction.REVERSE);
+        _motorRightRear.setDirection(DcMotor.Direction.REVERSE);
+
+        _sensorODS = hardwareMap.opticalDistanceSensor.get("ods");
+        _sensorLego = hardwareMap.lightSensor.get("light");
+
+        _manServo = hardwareMap.servo.get("manServo");
+    }
+
+    @Override
+    public void start() {
+        this.setEncoderMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+
+    }
+
+    @Override
+    public void loop() {
+
+        float pos = (gamepad1.left_stick_y + 1) / 2;
+        _manServo.setPosition(pos);
+        telemetry.addData("ServoPos", pos);
+        /*telemetry.addData("left front ", _motorLeftFront.getCurrentPosition());
+        telemetry.addData("right front", _motorRightFront.getCurrentPosition());
+        telemetry.addData("left rear  ", _motorLeftRear.getCurrentPosition());
+        telemetry.addData("right rear ", _motorRightRear.getCurrentPosition());*/
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    private void setEncoderMode(DcMotorController.RunMode mode) {
+        _motorLeftFront.setChannelMode(mode);
+        _motorRightFront.setChannelMode(mode);
+        _motorLeftRear.setChannelMode(mode);
+        _motorRightRear.setChannelMode(mode);
+    }
+}
